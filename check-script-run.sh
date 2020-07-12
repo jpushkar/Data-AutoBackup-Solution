950c2a02179206d63737cf6b6637b2cbce8d0d2f
jpushkar <66423213+jpushkar@users.noreply.github.com>
2020-07-13 00:02:33 +0530

    Update check-script-run.sh

--- check-script-run.sh
+++ check-script-run.sh
@@ -7,7 +7,7 @@ ps=`ps -ef | grep upload-volume2-17Sep.py | grep -v grep|awk -F " " '{print $2}'
 		echo "Script is not running. Restarting the script..."
 		cd /home/pushkar/python-code/upload-volume2/
 		nohup /usr/local/bin/python3.6 /home/pushkar/python-code/upload-volume2/upload-volume2-17Sep.py &
-		echo "Mumbai Backup : /volume2 upload script was not running started again.." | mail -s "Mumbai Backup: /volume2  " it.india@vuclip.com,pushkar.joshi@vuclip.com,suyog.shirgaonkar@vuclip.com,nirav.shah@vuclip.com 
+		echo " Backup : /volume2 upload script was not running started again.." | mail -s " Backup: /volume2  " myemail@domain.com 
 	else
 		echo "Script is running"
 	fi
